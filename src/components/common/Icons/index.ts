8ff108ddd4c0102e1fde15fedf84b97e3a359f0f
Thomas Lipari <thomas.lipari@gmail.com>
2019-04-19 15:37:40 -0700

    fix typo

--- src/components/common/Icons/index.ts
+++ src/components/common/Icons/index.ts
@@ -2,5 +2,5 @@ export { default as Cog } from './Cog.Icon';
 export { default as Folder } from './Folder.Icon';
 export { default as Grid } from './Grid.Icon';
 export { default as Infura } from './Infura.Icon';
-export { default as PowerOFf } from './PowerOFf.Icon';
+export { default as PowerOFf } from './PowerOff.Icon';
 export { default as Tachometer } from './Tachometer.Icon';
