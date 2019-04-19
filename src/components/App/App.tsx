f197a168a4b9471c32999fdc9226ef7c410f81fe
Thomas Lipari <thomas.lipari@gmail.com>
2019-04-19 12:39:31 -0700

    update App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import Content from '../Content';
+import { Web3ContextProvider } from '../../contexts/Web3Context';
+
 const App = () => (
-  <div />
+  <Web3ContextProvider>
+    <Content />
+  </Web3ContextProvider>
 );
 
 export default App;
