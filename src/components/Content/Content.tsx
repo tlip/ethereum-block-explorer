6dea0388c15b979e3da9f0f7e6fe76538e9e717e
Thomas Lipari <thomas.lipari@gmail.com>
2019-04-19 12:44:54 -0700

    add missing components

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Switch, Route, Redirect } from 'react-router';
+import ExplorerView from '../Views/ExplorerView/Loadable';
+import Nav from '../common/Nav';
+
+import './Content.scss';
+
+// Themed Component
+//
+const Content = () => (
+  <div className="content-container">
+    <Nav />
+    <Switch>
+      <Route path="/explorer" component={ExplorerView} />
+      <Route path="/" render={() => <Redirect to="/explorer" />} />
+    </Switch>
+  </div>
+);
+
+export default Content;
