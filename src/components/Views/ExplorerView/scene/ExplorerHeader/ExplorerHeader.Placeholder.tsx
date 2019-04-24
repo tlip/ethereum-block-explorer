532681d76d4fb6035dcca4ce01cf7100dd5b2fa6
Thomas Lipari <thomas.lipari@gmail.com>
2019-04-23 20:41:41 -0700

    import HeaderField loadable component in Header.Placeholder

--- src/components/Views/ExplorerView/scene/ExplorerHeader/ExplorerHeader.Placeholder.tsx
+++ src/components/Views/ExplorerView/scene/ExplorerHeader/ExplorerHeader.Placeholder.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Header from '../../../../common/Header';
-import HeaderField from '../../../../common/HeaderField';
+import HeaderField from '../../../../common/HeaderField/Loadable';
 
 const ExplorerHeaderPlaceholder = () => (
   <Header>
