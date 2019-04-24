30531cf46e43f88ef6910921bb5d5a1bc60108b2
Thomas Lipari <thomas.lipari@gmail.com>
2019-04-23 20:46:06 -0700

    incorporate statistics state to optimize average statistics in header

--- src/components/Views/ExplorerView/scene/ExplorerHeader/ExplorerHeader.tsx
+++ src/components/Views/ExplorerView/scene/ExplorerHeader/ExplorerHeader.tsx
@@ -3,31 +3,22 @@ import { Block, Transaction } from 'web3/eth/types';
 
 import ExplorerHeaderPlaceholder from './ExplorerHeader.Placeholder';
 import Header from '../../../../common/Header';
-import HeaderField from '../../../../common/HeaderField';
+import HeaderField from '../../../../common/HeaderField/Loadable';
 import thousandsFormat from '../../../../../utils/thousandsFormat';
 
-const ExplorerHeader = React.memo(({ blocks }: { blocks: Block[] }) => {
-  const averageBlockStats = { gasUsed: 0, fullness: 0, gasPrice: 0, length: 0 };
-  const currentBlock = blocks[0];
+const ExplorerHeader = React.memo(({ blocks, statistics }: {
+  blocks: Block[],
+  statistics: any,
+}) => {
+  const currentBlock = blocks.reverse()[0];
     
   // When we first launch the app, we fetch the 20 last blocks, causing this to go crazy.
   // So until the first 20 blocks are fetched, we'll just show a placeholder. 
-  if (!currentBlock || blocks.length < 19) {
+  if (!currentBlock || blocks.length < 10) {
     return (
       <ExplorerHeaderPlaceholder />
     );
   }
-
-  // Iterate through our blocks and aggregate some values to show some average statistics
-  blocks
-    .forEach((block: Block) => {
-      averageBlockStats.gasUsed += block.gasUsed;
-      averageBlockStats.fullness += (block.gasUsed / block.gasLimit);
-      block.transactions.forEach(({ gasPrice }: Transaction, i: number) => {
-        averageBlockStats.gasPrice += +gasPrice;
-        averageBlockStats.length++;
-      });
-    });
     
   return (
     <Header>
@@ -37,17 +28,17 @@ const ExplorerHeader = React.memo(({ blocks }: { blocks: Block[] }) => {
       />
       <HeaderField
         title="AVERAGE GAS PRICE"
-        value={((averageBlockStats.gasPrice / averageBlockStats.length) / 1000000000).toFixed(0)}
+        value={((statistics.gasPrice / statistics.transactionCount) / 1000000000).toFixed(0)}
         suffix="gwei"
       />
       <HeaderField
         title="AVERAGE BLOCK SIZE"
-        value={(averageBlockStats.gasUsed / blocks.length / 1000000).toFixed(1)}
+        value={(statistics.gasUsed / blocks.length / 1000000).toFixed(1)}
         suffix="mgas"
       />
       <HeaderField
         title="AVERAGE BLOCK FULLNESS"
-        value={`${(averageBlockStats.fullness / blocks.length * 100).toFixed(0)}%`}
+        value={`${(statistics.fullness / blocks.length * 100).toFixed(0)}%`}
       />
     </Header>
   );
