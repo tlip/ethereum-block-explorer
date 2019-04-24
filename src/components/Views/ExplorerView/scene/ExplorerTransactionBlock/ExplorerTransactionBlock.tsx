adfbcddb447a582fe753084a28288034e540a6bf
Thomas Lipari <thomas.lipari@gmail.com>
2019-04-23 20:44:36 -0700

    make tooltips more performant

--- src/components/Views/ExplorerView/scene/ExplorerTransactionBlock/ExplorerTransactionBlock.tsx
+++ src/components/Views/ExplorerView/scene/ExplorerTransactionBlock/ExplorerTransactionBlock.tsx
@@ -2,61 +2,78 @@ import React from 'react';
 
 import './ExplorerTransactionBlock.scss';
 
-const ExplorerTransactionBlock = (props: { transaction?: any }) => (
-  <div className="explorertransactionblock-container">
-    <div className="explorertransactionblock-block" />
+const ExplorerTransactionBlock = (props: { transaction?: any }) => {
 
-    {/* If this TransactioinBlock isn't a placeholder, then this is a tooltip. */}
-    {
-      !props.transaction || !props.transaction.hash 
-        ? null
-        : (
-          <div className="explorertransactionblock-tooltip">
-            <div className="explorertransactionblock-tooltip-top">
-              <div>
-                <div className="explorertransactionblock-tooltip-field">
-                  FROM
+  // isHovering state hook to track whether or not the component is being hovered on
+  const [isHovering, setHovering] = React.useState(false);
+
+  // Pre-parse the transaction value to reuse in 2 places
+  const parsedValue = props.transaction
+    ? props.transaction.value.slice(0, -13).slice(0, 5) / 10000
+    : 0;
+
+  return (
+    <div className="explorertransactionblock-container">
+      <div
+        className={`explorertransactionblock-block ${parsedValue && 'active'}`}
+        onMouseEnter={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+      />
+
+      {/**
+        * If this TransactioinBlock is being hovered on & isn't a placeholder,
+        * then this is a tooltip.
+        */
+        !isHovering || !props.transaction || !props.transaction.hash 
+          ? null
+          : (
+            <div className="explorertransactionblock-tooltip">
+              <div className="explorertransactionblock-tooltip-top">
+                <div>
+                  <div className="explorertransactionblock-tooltip-field">
+                    FROM
+                  </div>
+                  <div className="explorertransactionblock-tooltip-value">
+                    {
+                      props.transaction.from
+                        ? `${props.transaction.from.slice(0,6)}...${props.transaction.from.slice(-6)}`
+                        : null
+                    }
+                  </div>
                 </div>
-                <div className="explorertransactionblock-tooltip-value">
-                  {
-                    props.transaction.from
-                      ? `${props.transaction.from.slice(0,6)}...${props.transaction.from.slice(-6)}`
-                      : null
-                  }
+                <div>
+                  <div className="explorertransactionblock-tooltip-field">
+                    TO
+                  </div>
+                  <div className="explorertransactionblock-tooltip-value">
+                    {
+                      props.transaction.to
+                        ? `${props.transaction.to.slice(0,6)}...${props.transaction.to.slice(-6)}`
+                        : null
+                    }
+                  </div>
                 </div>
               </div>
-              <div>
-                <div className="explorertransactionblock-tooltip-field">
-                  TO
-                </div>
-                <div className="explorertransactionblock-tooltip-value">
-                  {
-                    props.transaction.to
-                      ? `${props.transaction.to.slice(0,6)}...${props.transaction.to.slice(-6)}`
-                      : null
-                  }
+              <div className="explorertransactionblock-tooltip-bottom">
+                <div>
+                  <div className="explorertransactionblock-tooltip-field">
+                    VALUE
+                  </div>
+                  <div className="explorertransactionblock-tooltip-value">
+                    {
+                      props.transaction.value
+                        ? parsedValue
+                        : null
+                    } ETH
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="explorertransactionblock-tooltip-bottom">
-              <div>
-                <div className="explorertransactionblock-tooltip-field">
-                  VALUE
-                </div>
-                <div className="explorertransactionblock-tooltip-value">
-                  {
-                    props.transaction.value
-                      ? +props.transaction.value.slice(0, -13).slice(0, 5) / 10000 
-                      : null
-                  } ETH
-                </div>
-              </div>
-            </div>
-          </div>
-        )
-    }
+          )
+      }
 
-  </div>
-);
+    </div>
+  );
+};
 
 export default ExplorerTransactionBlock;
