1f548ede582ea673db191c8688d88430c49d0b5e
Thomas Lipari <thomas.lipari@gmail.com>
2019-04-25 08:41:56 -0700

    use Loadable in ExplorerHeader

--- src/components/Views/ExplorerView/scene/ExplorerHeader/ExplorerHeader.Placeholder.tsx
+++ src/components/Views/ExplorerView/scene/ExplorerHeader/ExplorerHeader.Placeholder.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Header from '../../../../common/Header';
+import Header from '../../../../common/Header/Loadable';
 import HeaderField from '../../../../common/HeaderField/Loadable';
 
 const ExplorerHeaderPlaceholder = () => (
